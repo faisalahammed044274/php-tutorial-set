435e40be08d412e53c73f00bb226e687bb786ef1
faisalahammed044274 <faisalahammed044274@gmail.com>
2025-01-13 23:23:58 +0600

    PHP and HTML Output

---
+++ file-04-echo.php
@@ -0,0 +1,11 @@
+<?php
+
+echo "Hello, From echo!";
+echo "<br>";
+print "Hello, From print!";
+
+$name = "Faisal";
+print "<br>";
+print $name ." - Echo";
+echo "<br>";
+echo $name . " - Print";
---
+++ file-05-output.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form>
+<input type="text" name ="name" placeholder="Enter Name Here">
+<input type="submit" name="submit Form">
+</form>
+</br>
+<?php
+ echo '<input type="text" name ="name" placeholder="Enter Name Here">';
+ echo "<br>";
+  echo "<br>";
+  echo "<input type=\"text\" name =\"name\" placeholder=\"Enter Name Here\">";
+ echo '<input type="submit" name="submit Form">';
+?>
+
+</body>
+</html>
