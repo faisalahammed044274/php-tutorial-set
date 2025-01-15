fab2c918cc843ee47c9df7e3e2d1ea2ba005c63b
faisalahammed044274 <faisalahammed044274@gmail.com>
2025-01-15 15:02:14 +0600

    loop

--- file-16-loop.php
+++ file-16-loop.php
@@ -6,61 +6,63 @@
  *  code to be executed
  * }
  */
-echo "While Loop" . PHP_EOL;
+echo "While Loop => <br>" . PHP_EOL;
 
     $number = 1;
     while ($number <= 10) {
-        echo "The number is: $number" . PHP_EOL;
+        echo "The number is: $number <br>" . PHP_EOL;
         $number++;
     }
+
+    echo "<br>";
 
 /**
  * Do While Loop
  */
-echo "Do While Loop" . PHP_EOL;
+echo "Do While Loop => <br>" . PHP_EOL;
 
     $number = 1;
     do {
-        echo "The number is: $number" . PHP_EOL;
+        echo "The number is: $number <br>" . PHP_EOL;
         $number++;
     } while ($number <= 10);
 
-
+    echo "<br>";
 /**
  * For Loop
  */
-echo "For Loop" . PHP_EOL;
+echo "For Loop => <br>" . PHP_EOL;
 
     for ($i = 1; $i <= 10; $i++) {
-        echo "The number is: $i" . PHP_EOL;
+        echo "The number is: $i <br>" . PHP_EOL;
     }   
-
+    echo "<br>";
 /**
  * For Each Loop
  */
-echo "For Each Loop" . PHP_EOL;
+echo "For Each Loop => <br>" . PHP_EOL;
 
     $colors = array("red", "green", "blue", "yellow");
 
     foreach ($colors as $value) {
-        echo "$value" . PHP_EOL;
+        echo "$value <br>" . PHP_EOL;
     }
-
+    echo "<br>";
 /**
  * Break and Continue
  */
-echo "Break and Continue" . PHP_EOL;
+echo "Break and Continue => <br>" . PHP_EOL;
 
     for ($i = 0; $i <= 10; $i++) {
         if ($i == 5) {
             break;
         }
-        echo "The number is: $i" . PHP_EOL;
+        echo "The number is: $i <br>" . PHP_EOL;
     }
 
     for ($i = 0; $i <= 10; $i++) {
         if ($i == 5) {
             continue;
         }
-        echo "The number is: $i" . PHP_EOL;}
+        echo "The number is: $i <br>" . PHP_EOL;}
 ?>
