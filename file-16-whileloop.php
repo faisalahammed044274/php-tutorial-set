dcc12ec85d2f2f960c17b4cca2bfa38db533cd44
faisalahammed044274 <faisalahammed044274@gmail.com>
2025-01-15 13:59:44 +0600

    While Loop

---
+++ file-16-whileloop.php
@@ -0,0 +1,14 @@
+<?php
+
+/**While Loop
+ * 
+ * while (condition) {
+ *  code to be executed
+ * }
+ */
+
+    $number = 1;
+    while ($number <= 10) {
+        echo "The number is: $number" . PHP_EOL;
+        $number++;
+    }
