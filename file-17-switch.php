189423565c69986f6f02c8e8ce08742d623c9ef2
faisalahammed044274 <faisalahammed044274@gmail.com>
2025-01-16 19:50:06 +0600

    Switch

---
+++ file-17-switch.php
@@ -0,0 +1,44 @@
+<?php
+
+    $number = 33;
+
+ switch ($number) {
+     case 1:
+         echo "The number is 1";
+         break;
+     case 2:
+         echo "The number is 2";
+         break;
+     case 3:
+         echo "The number is 3";
+         break;
+     default:
+         echo "The number is not 1, 2, or 3";
+ }  
+echo PHP_EOL;
+
+ $day = "Sunday";
+ switch ($day){
+    case "Monday":
+        echo "Today is Monday";
+        break;
+    case "Tuesday":
+        echo "Today is Tuesday";
+        break;
+    case "Wednesday":
+        echo "Today is Wednesday";
+        break;
+    case "Thursday":
+        echo "Today is Thursday";
+        break;
+    case "Friday":
+        echo "Today is Friday";
+        break;
+    case "Saturday":
+        echo "Today is Saturday";
+        break;
+    case "Sunday":
+        echo "Today is Sunday! Day of rest!";
+        break;
+ }
+?>
