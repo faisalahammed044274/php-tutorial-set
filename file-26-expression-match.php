783f6255f8bf87468071206c4c76b1b07a3a1725
faisalahammed044274 <faisalahammed044274@gmail.com>
2025-01-22 20:24:49 +0600

    Expression Matching

---
+++ file-26-expression-match.php
@@ -0,0 +1,30 @@
+<?php
+$string = "This is a string";
+
+if(preg_match("/string/",$string)){
+    echo "Match found!";
+}else{
+    echo "No match found!";
+}
+
+
+echo PHP_EOL;
+
+
+function has_space($string){
+    if(preg_match('/ /', $string))
+    {
+        return true;
+    }else{
+        return false;
+    }       
+}
+
+if(has_space("This does not have space")){
+    echo "Has at least one space!";}
+else{
+    echo "Has no space!";
+}
+
+
+?>
