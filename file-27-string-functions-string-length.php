e672c1d32cc8c594b482944f53f488c59da4f744
faisalahammed044274 <faisalahammed044274@gmail.com>
2025-01-22 20:40:04 +0600

    String Functions

---
+++ file-27-string-functions-string-length.php
@@ -0,0 +1,14 @@
+<?php
+
+$data = "fghffdbfb4325ngbng@frvfb";
+$data_length = strlen($data);
+
+for($x=1; $x<=$data_length; $x++){
+    echo $x . "<br>";
+}   
+
+echo $data_length;
+
+
+
+?>
---
+++ file-28-string-upper-lower.php
@@ -0,0 +1,8 @@
+<?php
+
+
+$string = "I could eat bananas all day";
+echo strtoupper($string);
+echo PHP_EOL;
+echo strtolower($string);
+echo PHP_EOL;
