f72e6583a0af6b5506ed8e1b6436f5fb13a08d91
faisalahammed044274 <faisalahammed044274@gmail.com>
2025-02-05 16:36:17 +0600

    string function replacing predefined

--- file-35-string-function-replacing-predefined.php
+++ file-35-string-function-replacing-predefined.php
@@ -6,5 +6,6 @@ $string = "This is a string and This is an example project.";
 
 $string_new = str_replace('an example', 'a sample', $string);
 $string_new = str_replace('string', 'testing', $string_new);
-
+$string_new = str_replace('This', 'Which', $string_new);
+$string_new = substr_replace($string_new, 'Other', 23, 5);
 echo $string_new; // Outputs: "Th  a string, and an example."
