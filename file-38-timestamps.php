52cd1e81d100cb9c4fb14d13ffe70b6acc951831
faisalahammed044274 <faisalahammed044274@gmail.com>
2025-03-01 14:05:43 +0600

    timestamp

--- file-38-timestamps.php
+++ file-38-timestamps.php
@@ -17,11 +17,11 @@ echo "Timezone : ".date_default_timezone_set("Asia/Taipei")."<br>"; // Outputs t
 echo "Timezone : ".date_default_timezone_get()."<br>"; // Outputs the default timezonename 
 
 // Modifying the timezone
-date_default_timezone_set("Asia/Taipei");
-$t = time();
-echo  "Date : ".date("Y-m-d",$t); // Outputs the current date
+date_default_timezone_set("Asia/Taipei") . "<br>";
+$time = time();
+$time_now = date("Y-m-d H:i:s", $time);
+date_default_timezone_set("Asia/Taipei") . "<br>";
+echo "The current time is $time_now";
+$time_modified = date('D M Y @ H:i:s', $time+strtotime('+60 seconds'));
 echo "<br>";
-echo  "Time : ".date("H:i:s",$t); // Outputs the current time
-echo "<br>";
-$tz=timezone_open("Asia/Taipei");
-echo "Timezone : ".timezone_name_get($tz)."<br>"; // Outputs the timezone name
+echo 'Time now is : '. $time_now . '<br> The time 60 seconds from now is : ' . $time_modified;
