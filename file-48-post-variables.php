99dc6232ad5b6791fc2ff62379de3d709d6d72cc
faisalahammed044274 <faisalahammed044274@gmail.com>
2025-03-06 13:56:03 +0600

    Post Variables

---
+++ file-48-post-variables.php
@@ -0,0 +1,21 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    echo $name . '<br>';
+    echo $email . '<br>';
+    echo $password . '<br>';
+}else {
+    echo 'You can not browse this page directly';
+}
+
+?>
+
+<form action="" method="post">
+    <input type="text" name="name"><br>
+    <input type="email" name="email"> <br>
+    <input type="password" name="password" id=""><br>
+    <input type="submit" value="Submit">
+</form>
